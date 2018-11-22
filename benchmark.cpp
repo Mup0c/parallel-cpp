c30c40bc826f7c31e0d68919e6f2a81c259e1f84
Mup0c <mir-os@mail.ru>
2018-11-22 23:21:13 +1000

    commit to find repo:)

--- benchmark.cpp
+++ benchmark.cpp
@@ -54,6 +54,21 @@ static void BM_4_threads(benchmark::State& state) {
     }
 }
 
+static void BM_4_threads_no_pause(benchmark::State& state) {
+    for (auto _ : state){
+        vector<vector<double>> A(n), L(n), U(n), L_U(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                A[i].push_back(rand() % 20 - 10);
+                L[i].push_back(0);
+                U[i].push_back(0);
+                L_U[i].push_back(0);
+            }
+        }
+        LU_eval_threading(4, n, A, L, U);
+    }
+}
+
 static void BM_8_threads(benchmark::State& state) {
     for (auto _ : state){
         state.PauseTiming();
@@ -159,6 +174,7 @@ static void BM_71_threads(benchmark::State& state) {
 BENCHMARK(BM_1_thread);
 BENCHMARK(BM_2_threads);
 BENCHMARK(BM_4_threads);
+BENCHMARK(BM_4_threads_no_pause);
 BENCHMARK(BM_8_threads);
 BENCHMARK(BM_16_threads);
 BENCHMARK(BM_64_thread);
